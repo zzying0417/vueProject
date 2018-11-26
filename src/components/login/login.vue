1d589866180f1920f3bd862e07574bd5752648d8
zzy <305197842@qq.com>
2018-11-26 17:02:32 +0800

    login组件的样式

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,49 @@
 <template>
-    <div>登录组件</div>
+<div class='login-wrap'>
+    <el-form label-position="top" label-width="80px" :model="formData" class='form-login'>
+  <el-form-item label="用户名字">
+    <el-input v-model="formData.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="formData.username"></el-input>
+  </el-form-item>
+  <el-button type="primary" class="login-btn">登录</el-button>
+  
+</el-form>
+</div>
+   
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            formData:{
+                username:'',
+                password:''
+            }
+        }
+    }
 }
 </script>
 
 <style>
+.login-wrap{
+    height: 100%;
+    background-color: #324152;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+.form-login{
+    background-color: #fff;
+    width: 400px;
+    border-radius: 6px;
+    padding: 30px;
+    
+}
+.login-btn{
+    width: 100%;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 
 import ElementUI from 'element-ui'
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import './assets/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
